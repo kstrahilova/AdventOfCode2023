e8eb75cf87d52609d1922c089ae1e870c9751a6c
kstrahilova <kstrahilova@gmail.com>
2023-12-01 00:02:46 +0100

    template

---
+++ Template/Challenge.php
@@ -0,0 +1,23 @@
+<?php
+
+Challenge::main();
+
+class Challenge {
+function processLine(string $line)
+{
+    echo ($line);
+}
+
+function main()
+{
+    $input = file(__DIR__ . "/input.txt");
+    $result = 0;
+
+    foreach ($input as $line) {
+        Challenge::processLine($line);
+    }
+
+    echo ("Result: " . $result . "\n");
+}
+
+}
