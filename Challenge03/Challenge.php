6533a7150206a1b5f948828b63c978e64a07839b
kstrahilova <kstrahilova@gmail.com>
2023-12-03 15:17:06 +0100

    Challenge 3 works on example input

--- Challenge03/Challenge.php
+++ Challenge03/Challenge.php
@@ -4,51 +4,9 @@ Challenge::main();
 
 class Challenge
 {
-    function isSymbol(string $char) {
-        return !is_numeric($char) && $char !== ".";
-    }
-
-    function processString(string $string) : int {
-        echo $string . PHP_EOL;
-        
-        foreach (str_split($string) as $char) {
-            echo $char . PHP_EOL;
-            if (Challenge::isSymbol($char)) {
-                $number = preg_replace("/[^0-9]/", "", $string);
-                return (int) $number;
-            }
-        }
-        
-        return 0;
-    }
-
-    function processTuple(array $lineOne, array $lineTwo, array $lineThree) : int
+    function isSymbol(string $char): bool
     {
-        $number = "";
-        $result = 0;
-
-        for ($i = 0; $i < count($lineOne); $i++) {
-            $index = $i - 1;
-            while (is_numeric($lineOne[$i])) {
-                $i++;
-            }
-
-            while ($index <= $i) {
-                // echo "index is " . $index . PHP_EOL;
-                if ($index >= 0 && $index < count($lineOne)) {
-                    $number .= $lineOne[$index];
-                    $number .= $lineTwo[$index];
-                }
-                $index++;
-            }
-
-            $result += Challenge::processString($number);
-            $number = "";
-            $i = $index;
-
-        }
-        return $result;
-        
+        return !is_numeric($char) && $char !== ".";
     }
 
     function main()
@@ -56,19 +14,58 @@ class Challenge
         // $input = file(__DIR__ . "/input.txt");
         $input = file(__DIR__ . "/testInput.txt");
         $result = 0;
-        $lineOne = str_split(trim($input[0]));
-        $lineTwo = str_split(trim($input[1]));
-        $lineThree = str_split(trim($input[2]));
-        // $line = [$input[0], $input[1], $input[2]];
-        $result += Challenge::processTuple($lineOne, $lineTwo, $lineThree);
+        // $line = [$input[3], $input[4], $input[5]];
+        // $result += Challenge::processTuple($line);
 
 
-        // for ($i = 3; $i < count($input); $i++) {
+        // Loop over lines
+        for ($i = 0; $i < count($input); $i++) {
+            $line = str_split($input[$i]);
+            echo "Line is " . $input[$i] . PHP_EOL;
+            if (preg_replace("/[^0-9]/", "", $line) == "") {
+                continue;
+            }
+            $number = "";
+            // $string = "";
 
-        // }
+            //loop within line
+            for ($j = 0; $j < count($line); $j++) {
+                if (is_numeric($line[$j])) {
+                    if ($number == "") {
+                        // if number is empty, then this is the first digit
+                        $startIndex = ($j > 0) ? $j - 1 : $j;
+                        echo "start index is " . $startIndex . PHP_EOL;
+                    }
+                    $number .= $line[$j];
+                    // if i need to debug, remove this;
+                    // Set read digits to . s.t. they don't get counted more than once
+                    $line[$j] = '.';
 
-        echo $input[0];
-        echo $input[1];
+                } else if ($number !== "") {
+                    // then we have just read the number and we are at the symbol after it
+                    // load the symbols around it
+                    echo "Number is " . $number . PHP_EOL;
+                    for ($index = $startIndex; $index <= $j; $index++) {
+                        $startRow = ($i > 0) ? $i - 1 : $i;
+                        $endRow = ($i < count($input) - 1) ? $i + 1 : $i;
+
+                        for ($k = $startRow; $k <= $endRow; $k++) {
+
+                            $char = trim(str_split($input[$k])[$index]);
+                            if ($char !== "" && Challenge::isSymbol($char)) {
+                                echo "Adding " . $number . " char is " . $char . PHP_EOL;
+                                $result += (int) $number;
+                                $number = '';
+
+                            }
+                        }
+                    }
+
+
+
+                }
+            }
+        }
 
         echo ("Result: " . $result . "\n");
     }
