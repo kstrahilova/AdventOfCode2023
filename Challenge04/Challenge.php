9fe93b6e319d418077c7472d61a7be962596624f
Karolina Strahilova <karolina.strahilova@wolfpackit.nl>
2023-12-04 09:00:44 +0100

    start part 2

--- Challenge04/Challenge.php
+++ Challenge04/Challenge.php
@@ -4,9 +4,11 @@ Challenge::main();
 
 class Challenge
 {
-    static function processLine(string $line) : int
+    static function processLine(string $line) : array // int
     {
-        list($winning, $actual) = explode(' | ', trim(explode(': ', trim($line))[1]));
+        $line = explode(': ', trim($line));
+        $card = explode(' ', $line[0])[1];
+        list($winning, $actual) = explode(' | ', $line[1]);
         $winning = explode(' ', $winning);
         array_walk($winning, fn (string $string) => trim($string));
         $actual = explode(' ', $actual);
@@ -18,18 +20,38 @@ class Challenge
             }
         }
 
-        $result = ($matches > 0) ? 2 ** ($matches - 1) : 0;
+        // $result = ($matches > 0) ? 2 ** ($matches - 1) : 0;
+        // return $result;
+        echo "Card " . $card . " matches " . $matches . PHP_EOL;
+        return [$card, $matches];
+    }
+
+    static function processCard(array $cards, int $current) : int {
+        $matches = $cards[$current];
+        $result = 1;
+
+        for ($i = $current + 1; $i <= $current + $matches; $i++) {
+            $result += 1 + Challenge::processCard($cards, $i);
+        }
+
         return $result;
     }
 
     static function main()
     {
-        $input = file(__DIR__ . "/input.txt");
-        // $input = file(__DIR__ . "/testInput.txt");
+        // $input = file(__DIR__ . "/input.txt");
+        $input = file(__DIR__ . "/testInput.txt");
         $result = 0;
+        $cards = [];
 
         foreach ($input as $line) {
-            $result += Challenge::processLine($line);
+            // $result += Challenge::processLine($line);
+            $card = Challenge::processLine($line);
+            $cards[$card[0]] = $card[1];
+        }
+
+        for ($i = 1; $i <= count($cards); $i++) {
+            $result += Challenge::processCard($cards, $i);
         }
 
         echo ("Result: " . $result . "\n");
