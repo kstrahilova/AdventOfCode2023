724ee08d20a92004fdcd8910037904526e5e30e4
kstrahilova <kstrahilova@gmail.com>
2023-12-05 09:32:51 +0100

    challenge 05 part one

--- Challenge05/Challenge.php
+++ Challenge05/Challenge.php
@@ -8,34 +8,44 @@ class Challenge
     {
         list($destStart, $sourceStart, $range) = explode(' ', $line);
         $destStart = (int) $destStart;
+        // $destEnd = $destStart = $range - 1;
         $sourceStart = (int) $sourceStart;
+        $sourceEnd = $sourceStart + $range - 1;
         $range = (int) $range;
-        // echo 'Source start is ', $sourceStart, ' dest start is ', $destStart, " range is ", $range, PHP_EOL;
-        echo $destStart, " ", $sourceStart, " ", $range, PHP_EOL;
 
-        for ($i = $range - 1; $i >= 0; $i--) {
-            $source = $sourceStart + $i;
-            $dest = $destStart + $i;
-            if (in_array($source, $sourceArray)) {
-                // echo 'Source is ', $source, ' dest is ', $dest, PHP_EOL;
-                // if (array_count_values($mapping)[$source] > 1) {
-                //     echo "Issue " . PHP_EOL;
-                // }
+        foreach ($sourceArray as $source) {
+            if ($sourceStart <= $source && $source <= $sourceEnd) {
                 $sourceIndex = array_search($source, $sourceArray);
+                $dest = $destStart + $source - $sourceStart;
                 $destArray[$sourceIndex] = $dest;
-                echo "Map " . $source . " -> " . $dest . PHP_EOL;
+                // echo "Map " . $source . " -> " . $dest . PHP_EOL;
             }
-
         }
+
 
         foreach ($sourceArray as $index => $element) {
             if (!array_key_exists($index, $destArray)) {
                 $destArray[$index] = $element;
+                // echo "Not present, Map " . $element . " -> " . $element . PHP_EOL;
             }
         }
         return $destArray;
 
     }
+
+    // function processSeeds(array $input) : array {
+    //     $seeds = [];
+    //     for ($i = 0; $i < count($input); $i++) {
+    //         $start = $input[$i];
+    //         $i += 1;
+    //         $range = $input[$i];
+    //         for ($j = $start; $j < $range + $start; $j++) {
+    //             echo 'j = ', $j,PHP_EOL;
+    //             $seeds[] = $j;
+    //         }
+    //     }
+    //     return $seeds;
+    // }
 
     function main()
     {
@@ -56,19 +66,18 @@ class Challenge
             $line = trim($input[$i]);
             $exploded = explode(':', $line);
             $firstWord = trim($exploded[0]);
-            // echo "First word is " . $firstWord . PHP_EOL;
             if ($firstWord == 'seeds') {
                 $seedsInput = explode(' ', trim($exploded[1]));
                 foreach ($seedsInput as $seed) {
                     $seeds[] = $seed;
                 }
+                // $seeds = Challenge::processSeeds($seedsInput);
             } else if ($firstWord == "seed-to-soil map") {
                 echo "\n" . $firstWord . PHP_EOL;
                 $i += 1;
                 $line = trim($input[$i]);
                 $soil = [];
                 while ($line !== "") {
-                    // echo " test " . PHP_EOL;
                     $soil = Challenge::processMapping($line, $seeds, $soil);
 
                     $i += 1;
@@ -80,7 +89,6 @@ class Challenge
                 $line = trim($input[$i]);
                 $fert = [];
                 while ($line !== "") {
-                    // echo " test " . PHP_EOL;
                     $fert = Challenge::processMapping($line, $soil, $fert);
 
                     $i += 1;
@@ -92,7 +100,6 @@ class Challenge
                 $line = trim($input[$i]);
                 $water = [];
                 while ($line !== "") {
-                    // echo " test " . PHP_EOL;
                     $water = Challenge::processMapping($line, $fert, $water);
 
                     $i += 1;
@@ -104,7 +111,6 @@ class Challenge
                 $line = trim($input[$i]);
                 $light = [];
                 while ($line !== "") {
-                    // echo " test " . PHP_EOL;
                     $light = Challenge::processMapping($line, $water, $light);
 
                     $i += 1;
@@ -116,7 +122,6 @@ class Challenge
                 $line = trim($input[$i]);
                 $temp = [];
                 while ($line !== "") {
-                    // echo " test " . PHP_EOL;
                     $temp = Challenge::processMapping($line, $light, $temp);
 
                     $i += 1;
@@ -128,7 +133,6 @@ class Challenge
                 $line = trim($input[$i]);
                 $humidity = [];
                 while ($line !== "") {
-                    // echo " test " . PHP_EOL;
                     $humidity = Challenge::processMapping($line, $temp, $humidity);
 
                     $i += 1;
@@ -140,7 +144,6 @@ class Challenge
                 $line = trim($input[$i]);
                 $location = [];
                 while ($line !== "") {
-                    // echo " test " . PHP_EOL;
                     $location = Challenge::processMapping($line, $humidity, $location);
 
                     $i += 1;
