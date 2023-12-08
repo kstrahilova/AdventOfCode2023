f09f30c2f5b740f0a088ec443b8e6ceaab886de0
kstrahilova <kstrahilova@gmail.com>
2023-12-08 12:28:43 +0100

    Day 8 part 2

--- Challenge08/Challenge.php
+++ Challenge08/Challenge.php
@@ -9,14 +9,36 @@ class Challenge
         echo ($line);
     }
 
+    static function gcd(int $a, int $b)
+    {
+        while ($b !== 0) {
+            $temp = $b;
+            $b = $a % $b;
+            $a = $temp;
+        }
+
+        return $a;
+    }
+
+    static function lcm(int $a, int $b)
+    {
+        return $a * $b / Challenge::gcd($a, $b);
+    }
+
     static function main()
     {
-        $input = file(__DIR__ . "/testInput.txt");
-        // $input = file(__DIR__ . "/input.txt");
-        $result = 0;
+        // $input = file(__DIR__ . "/testInput.txt");
+        $input = file(__DIR__ . "/input.txt");
+        $part = 2;
+
+        // Part 1
         $firstLocation = "AAA";
         $currentLocation = $firstLocation;
         $lastLocation = "ZZZ";
+
+        // Part 2
+        $firstLocations = [];
+
         $instructions = array_map(fn($instruction) => ($instruction == 'L') ? 0 : 1, str_split(trim($input[0])));
         $instrSize = count($instructions);
         $map = [];
@@ -27,30 +49,45 @@ class Challenge
             $line[1] = str_replace(array("(", ")", ","), "", trim($line[1]));
             $coordinates = explode(" ", trim($line[1]));
             $map[$index] = $coordinates;
+
+            if ($part == 2 && str_split($index)[2] == 'A') {
+                $firstLocations[] = $index;
+            }
         }
 
         $index = 0;
 
-        while ($currentLocation != $lastLocation) {
-            $result += 1;
-            $instruction = $instructions[$index];
-            // echo "Instruction index " . $instruction . "\n";
-            // echo "current location " . $currentLocation . "\n";
-            $currentLocation = $map[$currentLocation][$instruction];
-            $index = ($index + 1) % $instrSize;
+        if ($part == 1) {
+            $result = 0;
+            while ($currentLocation != $lastLocation) {
+                $result += 1;
+                $instruction = $instructions[$index];
+                $currentLocation = $map[$currentLocation][$instruction];
+                $index = ($index + 1) % $instrSize;
+            }
+
+        } else {
+            for ($loc = 0; $loc < count($firstLocations); $loc++) {
+                $currentLocation = $firstLocations[$loc];
+                $path = 0;
+                while (str_split($currentLocation)[2] !== 'Z') {
+                    $path += 1;
+                    $instruction = $instructions[$index];
+                    $currentLocation = $map[$currentLocation][$instruction];
+                    $index = ($index + 1) % $instrSize;
+                }
+                $paths[] = $path;
+            }
+
+            foreach ($paths as $path) {
+                if (!isset($result)) {
+                    $result = $path;
+                } else {
+                    $result = Challenge::lcm($result, $path);
+                }
+            }
+
         }
-
-        // foreach ($instructions as $instruction) {
-        //     echo $instruction . " ";
-        // }
-        // echo "\n";
-        // foreach ($map as $i => $c) {
-        //     echo $i . " => (" . $c[0] . ", " . $c[1] . ") \n";
-        // }
-
-        // echo $map[$firstLocation][0] . PHP_EOL;
-        // $index = $instructions[0];
-        // echo $map[$firstLocation][$index] . PHP_EOL;
 
         echo ("Result: " . $result . "\n");
     }
